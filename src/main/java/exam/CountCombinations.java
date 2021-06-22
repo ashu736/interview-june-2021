3ce985a3ce151da8489f311419c483a197f8ea10
Ashish Gaikwad <70815139+ashu736@users.noreply.github.com>
2021-06-22 13:13:18 +0530

    Update CountCombinations.java

--- src/main/java/exam/CountCombinations.java
+++ src/main/java/exam/CountCombinations.java
@@ -9,20 +9,20 @@ static void printVector(ArrayList<Integer> arr)
         count++;
     }
 }
-  static void findWays(ArrayList<Integer> arr, int i, int n)
+  static void findWays(ArrayList<Integer> arr, int i, int n,int k)
 {
     if (n == 0)
        printVector(arr);
-    for(int j = i; j <= n; j++)
+    for(int j = i; j <= n && j<=k; j++)
     {
         arr.add(j);
-        findWays(arr, j, n - j);
+        findWays(arr, j, n - j, k);
         arr.remove(arr.size() - 1);
     }
 }
   public int getNumberOfWays(final int total, final int k) {
     ArrayList<Integer> arr = new ArrayList<Integer>();
-    findWays(arr, 1, total);
+    findWays(arr, 1, total, k);
     return count;
   }
 
